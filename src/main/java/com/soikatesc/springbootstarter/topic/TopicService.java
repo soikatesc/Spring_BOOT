72a4dc482b093edb79892b3c46a4dbf853c037ef
Md Rahaman <soikatesc@gmail.com>
2017-08-12 18:08:01 -0500

    Spring Boot application with Topic controller and service

---
+++ src/main/java/com/soikatesc/springbootstarter/topic/TopicService.java
@@ -0,0 +1,44 @@
+package com.soikatesc.springbootstarter.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	// it's a singleton
+	private List<Topic> topics =  new ArrayList<>(Arrays.asList(
+			new Topic("spring", "Spring Framework", "Spring Framework description"),
+			new Topic("java", "Spring Framework", "Spring Framework description"),
+			new Topic("javascript", "Spring Framework", "Spring Framework description")
+			));
+	
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+	
+	public void updateTopic(String id, Topic topic) {
+		for(int i=0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+	
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
+	}
+}

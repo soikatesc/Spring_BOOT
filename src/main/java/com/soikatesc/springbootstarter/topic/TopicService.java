cbe7ba6f6040f5f0685136c77787e26f43b03bac
Md Rahaman <soikatesc@gmail.com>
2017-08-12 18:29:43 -0500

    completed CRUD operation for Topics

--- src/main/java/com/soikatesc/springbootstarter/topic/TopicService.java
+++ src/main/java/com/soikatesc/springbootstarter/topic/TopicService.java
@@ -4,41 +4,52 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
 	
+	@Autowired
+	private TopicRepository topicRepository;
 	// it's a singleton
-	private List<Topic> topics =  new ArrayList<>(Arrays.asList(
-			new Topic("spring", "Spring Framework", "Spring Framework description"),
-			new Topic("java", "Spring Framework", "Spring Framework description"),
-			new Topic("javascript", "Spring Framework", "Spring Framework description")
-			));
+//	private List<Topic> topics =  new ArrayList<>(Arrays.asList(
+//			new Topic("spring", "Spring Framework", "Spring Framework description"),
+//			new Topic("java", "Spring Framework", "Spring Framework description"),
+//			new Topic("javascript", "Spring Framework", "Spring Framework description")
+//			));
 	
 	public List<Topic> getAllTopics() {
+//		return topics;
+		List<Topic> topics = new ArrayList<>();
+		topicRepository.findAll()
+		.forEach(topics::add);
 		return topics;
 	}
 	
 	public Topic getTopic(String id) {
-		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+//		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		return topicRepository.findOne(id);
 	}
 	
 	public void addTopic(Topic topic) {
-		topics.add(topic);
+		topicRepository.save(topic);
 	}
 	
 	public void updateTopic(String id, Topic topic) {
-		for(int i=0; i < topics.size(); i++) {
-			Topic t = topics.get(i);
-			if(t.getId().equals(id)) {
-				topics.set(i, topic);
-				return;
-			}
-		}
+//		for(int i=0; i < topics.size(); i++) {
+//			Topic t = topics.get(i);
+//			if(t.getId().equals(id)) {
+//				topics.set(i, topic);
+//				return;
+//			}
+//		}
+		
+		topicRepository.save(topic);
 	}
 	
 	public void deleteTopic(String id) {
-		topics.removeIf(t -> t.getId().equals(id));
+//		topics.removeIf(t -> t.getId().equals(id));
+		topicRepository.delete(id);
 	}
 }
